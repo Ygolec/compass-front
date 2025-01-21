8ca0dc2717e06a4a323b8702f1d1bee01de44c04
Daniil <thekevindit@gmail.com>
2025-01-21 11:33:59 +0300

    Логика авторизации и окно регистрации

---
+++ components/layouts/default/avatar_menu.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-menu
+      v-model="menu"
+      :close-on-content-click="false"
+      location="bottom"
+      transition="slide-y-transition"
+  >
+    <template v-slot:activator="{ props }">
+      <v-avatar
+          color="surface-variant"
+          size="50"
+          v-bind="props"
+      >
+      </v-avatar>
+    </template>
+
+    <v-card
+        class="mt-2"
+        min-width="200">
+
+      <v-card-actions>
+        <v-btn
+            href="/login"
+            block
+        >
+          Войти
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-menu>
+</template>
+<script setup lang="ts">
+const menu = ref<boolean>(false);
+
+</script>
+
+<style scoped>
+
+</style>
