da05fa7dbf7e4f9efc8f4a13a84d0263302871e0
Daniil <thekevindit@gmail.com>
2025-02-21 20:23:10 +0300

    env переменные, не подтягиваются в среде, если указать однозначное обозначение для переменных

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,13 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     runtimeConfig:{
-        HSE_EMAIL: process.env.HSE_EMAIL,
-        HSE_EMAIL_PASSWORD: process.env.HSE_EMAIL_PASSWORD,
-        ENCRYPTION_KEY_FOR_HSE_TOKEN: process.env.ENCRYPTION_KEY_FOR_HSE_TOKEN,
-        DATABASE_URL: process.env.DATABASE_URL,
-        DIRECTUS_URL: process.env.DIRECTUS_URL,
-        DIRECTUS_TOKEN: process.env.DIRECTUS_TOKEN,
-        AUTH_BACKEND_URL: process.env.AUTH_BACKEND_URL,
+        HSE_EMAIL: '',
+        HSE_EMAIL_PASSWORD: '',
+        ENCRYPTION_KEY_FOR_HSE_TOKEN: '',
+        DATABASE_URL: '',
+        DIRECTUS_URL: '',
+        DIRECTUS_TOKEN: '',
+        AUTH_BACKEND_URL: '',
     },
     compatibilityDate: '2024-04-03',
     build: {
